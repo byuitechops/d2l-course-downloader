c4a23daff06d0a7b752ba5ca12a56d777c46cdb7
Zachary Williams <wil11063@byui.edu>
2017-10-09 15:37:38 -0600

    Slapped a bandaid on my mistakes

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-module.exports = () => {
 
-};
+var getPrompt = require('./prompt.js');
+var fs = require('fs');
+var path = require('path');
+var d3dsv = require('./d3-dsv');
+
+var csvFile = '';
+
+getPrompt((err, promptData) => {
+  fs.readFile(promptData.csvName, (err, data) => {
+
+    console.log(data);
+  });
+});
--- prompt.js
+++ prompt.js
@@ -1,3 +1,63 @@
-module.exports = () => {
+var prompt = require('prompt');
+
+var promptSettings = [{
+    name: 'username',
+    type: 'string',
+    description: 'Enter your username',
+    required: true,
+    message: 'Username cannot be empty.'
+  },
+  {
+    name: 'password',
+    description: 'Enter your password',
+    type: 'string',
+    required: true,
+    hidden: true,
+    replace: '*',
+    message: 'Password cannot be empty.'
+  },
+  {
+    name: 'csvName',
+    type: 'string',
+    description: 'Enter your the name of the CSV',
+    required: true
+  },
+  {
+    name: 'maxConcurrent',
+    type: 'number',
+    description: 'Enter the max number of concurrent downloads allowed',
+    required: true,
+    default: '50',
+    message: 'Must be a number.'
+  },
+  {
+    name: 'subdomain',
+    type: 'string',
+    description: 'Is this for pathway? (yes/no)',
+    required: true,
+    before: (value) => {
+      if (value.toLowerCase() != 'yes') {
+        return 'no';
+      } else {
+        return value;
+      }
+    }
+  },
+];
+
+module.exports = (callback) => {
+
+  function promptCB(err, promptData) {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    callback(null, promptData);
+  }
+
+  prompt.get(promptSettings, promptCB);
+
+  prompt.start();
 
 };
