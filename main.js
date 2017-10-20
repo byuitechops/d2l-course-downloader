5247f0f40546f171f94d34415123ac2a35e660bc
Zachary Williams <wil11063@byui.edu>
2017-10-20 15:39:49 -0600

    Fo dans

--- main.js
+++ main.js
@@ -10,77 +10,94 @@ var dlCourse = require('./dlCourse.js');
 var chalk = require('chalk');
 
 module.exports = (promptData) => {
+    console.log(promptData);
 
     var domain = promptData.subdomain === 'yes' ? 'pathway' : 'byui';
 
-    if (promptData.source.includes('.csv')) {
+    /* Determine settings */
+    var settings = {
+        userName: promptData.userName,
+        userNameSelector: '#userName',
+        password: promptData.password,
+        passwordSelector: '#password',
+        doneButtonSelector: '[primary="primary"]',
+        loginURL: `https://${domain}.brightspace.com/d2l/login?noredirect=1`,
+        afterLoginURL: `https://${domain}.brightspace.com/d2l/home`
+    };
+
+    function getCSVData(callback) {
         /* Read in the CSV */
-        fs.readFile(path.join('.', promptData.csvName), 'utf8', (err, data) => {
+        fs.readFile(path.join('.', promptData.source), 'utf8', (err, data) => {
             if (err) {
                 console.log(err);
+                callback(err, null);
             } else {
-
                 /* Parse the CSV */
                 var csvData = d3.csvParse(data);
-                // console.log(csvData);
+                callback(null, csvData);
+            }
+        });
+    }
 
-                /* Determine settings */
-                var settings = {
-                    userName: promptData.userName,
-                    userNameSelector: '#userName',
-                    password: promptData.password,
-                    passwordSelector: '#password',
-                    doneButtonSelector: '[primary="primary"]',
-                    loginURL: `https://${domain}.brightspace.com/d2l/login?noredirect=1`,
-                    afterLoginURL: `https://${domain}.brightspace.com/d2l/home`
-                };
-
-                /* Get them cookies */
-                getCookies(settings, (errorCookies, cookies) => {
-                    if (errorCookies) {
-                        console.log(chalk.red(errorCookies));
-                    } else {
-                        // console.log(csvData);
-                        /* Set parameters for download */
-                        var courses = csvData.map(course => {
-                            return {
-                                domain: domain,
-                                ou: course.OU,
-                                name: '',
-                                nightmareCookies: cookies,
-                                loginURL: settings.loginURL,
-                                userName: promptData.userName,
-                                password: promptData.password,
-                                devtools: true,
-                            };
-                        });
-                        /* Download ALL the courses */
-                        asyncLib.mapLimit(courses, promptData.maxConcurrent, dlCourse, (
-                            mapError, results) => {
-                            var stringified = results.map(result => JSON.stringify(
-                                result));
-                            fs.writeFile('./results.json', stringified, errorFS => {
-                                if (errorFS) {
-                                    console.log(chalk.red(errorFS));
-                                } else {
-                                    var failList = results.filter(course =>
-                                        !course.success);
-                                    if (failList.length === 0) {
-                                        console.log(chalk.green(
-                                            `All ${chalk.yellow(results.length)} courses successfully downloaded.`
-                                        ));
-                                    } else {
-                                        console.log(chalk.redBright(
-                                            `These ${chalk.red(failList.length)} courses failed to download:`
-                                        ));
-                                        console.log(failList);
-                                    }
-                                }
-                            });
-                        });
-                    }
+    function downloadCourses(courseList) {
+        /* Get them cookies */
+        getCookies(settings, (errorCookies, cookies) => {
+            if (errorCookies) {
+                console.log(chalk.red(errorCookies));
+            } else {
+                /* Set parameters for download */
+                var courses = courseList.map(course => {
+                    return {
+                        domain: domain,
+                        ou: course.OU,
+                        name: '',
+                        nightmareCookies: cookies,
+                        loginURL: settings.loginURL,
+                        userName: promptData.userName,
+                        password: promptData.password,
+                        devtools: true,
+                    };
+                });
+                /* Download ALL the courses */
+                asyncLib.mapLimit(courses, promptData.maxConcurrent, dlCourse, (
+                    mapError, results) => {
+                    var stringified = results.map(result => JSON.stringify(
+                        result));
+                    fs.writeFile('./results.json', stringified, errorFS => {
+                        if (errorFS) {
+                            console.log(chalk.red(errorFS));
+                        } else {
+                            var failList = results.filter(course =>
+                                !course.success);
+                            if (failList.length === 0) {
+                                console.log(chalk.green(
+                                    `All ${chalk.yellow(results.length)} courses successfully downloaded.`
+                                ));
+                            } else {
+                                console.log(chalk.redBright(
+                                    `These ${chalk.red(failList.length)} courses failed to download:`
+                                ));
+                                console.log(failList);
+                            }
+                        }
+                    });
                 });
             }
         });
+    }
+
+    if (promptData.source.includes('.csv')) {
+        getCSVData((d3Error, csvData) => {
+            if (d3Error) {
+                console.error(d3Error);
+                return;
+            } else {
+                downloadCourses(csvData);
+            }
+        });
+    } else {
+        downloadCourses([{
+            OU: promptData.source
+        }]);
     }
 }
