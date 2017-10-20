63e1ad6c246c02337188617d555286c8f702640c
Danverde <gre12014@byui.edu>
2017-10-20 15:05:25 -0600

    Prompt is different

--- main.js
+++ main.js
@@ -13,72 +13,74 @@ module.exports = (promptData) => {
 
     var domain = promptData.subdomain === 'yes' ? 'pathway' : 'byui';
 
-    /* Read in the CSV */
-    fs.readFile(path.join('.', promptData.csvName), 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
+    if (promptData.source.includes('.csv')) {
+        /* Read in the CSV */
+        fs.readFile(path.join('.', promptData.csvName), 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
 
-            /* Parse the CSV */
-            var csvData = d3.csvParse(data);
-            // console.log(csvData);
+                /* Parse the CSV */
+                var csvData = d3.csvParse(data);
+                // console.log(csvData);
 
-            /* Determine settings */
-            var settings = {
-                userName: promptData.userName,
-                userNameSelector: '#userName',
-                password: promptData.password,
-                passwordSelector: '#password',
-                doneButtonSelector: '[primary="primary"]',
-                loginURL: `https://${domain}.brightspace.com/d2l/login?noredirect=1`,
-                afterLoginURL: `https://${domain}.brightspace.com/d2l/home`
-            };
+                /* Determine settings */
+                var settings = {
+                    userName: promptData.userName,
+                    userNameSelector: '#userName',
+                    password: promptData.password,
+                    passwordSelector: '#password',
+                    doneButtonSelector: '[primary="primary"]',
+                    loginURL: `https://${domain}.brightspace.com/d2l/login?noredirect=1`,
+                    afterLoginURL: `https://${domain}.brightspace.com/d2l/home`
+                };
 
-            /* Get them cookies */
-            getCookies(settings, (errorCookies, cookies) => {
-                if (errorCookies) {
-                    console.log(chalk.red(errorCookies));
-                } else {
-                    // console.log(csvData);
-                    /* Set parameters for download */
-                    var courses = csvData.map(course => {
-                        return {
-                            domain: domain,
-                            ou: course.OU,
-                            name: '',
-                            nightmareCookies: cookies,
-                            loginURL: settings.loginURL,
-                            userName: promptData.userName,
-                            password: promptData.password,
-                            devtools: true,
-                        };
-                    });
-                    /* Download ALL the courses */
-                    asyncLib.mapLimit(courses, promptData.maxConcurrent, dlCourse, (
-                        mapError, results) => {
-                        var stringified = results.map(result => JSON.stringify(
-                            result));
-                        fs.writeFile('./results.json', stringified, errorFS => {
-                            if (errorFS) {
-                                console.log(chalk.red(errorFS));
-                            } else {
-                                var failList = results.filter(course =>
-                                    !course.success);
-                                if (failList.length === 0) {
-                                    console.log(chalk.green(
-                                        `All ${chalk.yellow(results.length)} courses successfully downloaded.`
-                                    ));
-                                } else {
-                                    console.log(chalk.redBright(
-                                        `These ${chalk.red(failList.length)} courses failed to download:`
-                                    ));
-                                    console.log(failList);
-                                }
-                            }
+                /* Get them cookies */
+                getCookies(settings, (errorCookies, cookies) => {
+                    if (errorCookies) {
+                        console.log(chalk.red(errorCookies));
+                    } else {
+                        // console.log(csvData);
+                        /* Set parameters for download */
+                        var courses = csvData.map(course => {
+                            return {
+                                domain: domain,
+                                ou: course.OU,
+                                name: '',
+                                nightmareCookies: cookies,
+                                loginURL: settings.loginURL,
+                                userName: promptData.userName,
+                                password: promptData.password,
+                                devtools: true,
+                            };
                         });
-                    });
-                }
-            });
-        }
-    });
+                        /* Download ALL the courses */
+                        asyncLib.mapLimit(courses, promptData.maxConcurrent, dlCourse, (
+                            mapError, results) => {
+                            var stringified = results.map(result => JSON.stringify(
+                                result));
+                            fs.writeFile('./results.json', stringified, errorFS => {
+                                if (errorFS) {
+                                    console.log(chalk.red(errorFS));
+                                } else {
+                                    var failList = results.filter(course =>
+                                        !course.success);
+                                    if (failList.length === 0) {
+                                        console.log(chalk.green(
+                                            `All ${chalk.yellow(results.length)} courses successfully downloaded.`
+                                        ));
+                                    } else {
+                                        console.log(chalk.redBright(
+                                            `These ${chalk.red(failList.length)} courses failed to download:`
+                                        ));
+                                        console.log(failList);
+                                    }
+                                }
+                            });
+                        });
+                    }
+                });
+            }
+        });
+    }
 }
--- prompt.js
+++ prompt.js
@@ -1,52 +1,54 @@
+/*eslint-env node, es6*/
+/*eslint no-console:1*/
+
 var prompt = require('prompt');
-var chalk = require('chalk');
 var main = require('./main.js');
 
 var promptSettings = [{
-    name: 'userName',
-    type: 'string',
-    description: 'Enter your username',
-    required: true,
-    message: 'Username cannot be empty.'
+        name: 'userName',
+        type: 'string',
+        description: 'Enter your username',
+        required: true,
+        message: 'Username cannot be empty.'
   },
-  {
-    name: 'password',
-    description: 'Enter your password',
-    type: 'string',
-    required: true,
-    hidden: true,
-    replace: '*',
-    message: 'Password cannot be empty.'
+    {
+        name: 'password',
+        description: 'Enter your password',
+        type: 'string',
+        required: true,
+        hidden: true,
+        replace: '*',
+        message: 'Password cannot be empty.'
   },
-  {
-    name: 'csvName',
-    type: 'string',
-    description: 'Enter your the name of the CSV',
-    required: true
+    {
+        name: 'source',
+        type: 'string',
+        description: 'Enter the D2L OU or the name of the CSV',
+        required: true,
+        pattern: /(\d+)|(\S+.csv$)/
   },
-  {
+    {
+        name: 'subdomain',
+        type: 'string',
+        description: 'Is this for pathway? (yes/no)',
+        before: (value) => {
+            if (value.toLowerCase() != 'yes' || value.toLowerCase() != 'y') {
+                return 'no';
+            } else {
+                return 'yes';
+            }
+        }
+  },
+];
+
+var downloadMax = [{
     name: 'maxConcurrent',
     type: 'number',
     description: 'Enter the max number of concurrent downloads allowed',
     required: true,
     default: '15',
     message: 'Must be a number.'
-  },
-  {
-    name: 'subdomain',
-    type: 'string',
-    description: 'Is this for pathway? (yes/no)',
-    before: (value) => {
-      if (value.toLowerCase() != 'yes' || value.toLowerCase() != 'y') {
-        return 'no';
-      } else {
-        return 'yes';
-      }
-    }
-  },
-];
-
-// prompt.start();
+}];
 
 prompt.get(promptSettings, (err, promptData) => {
     if (err) {
@@ -54,5 +56,17 @@ prompt.get(promptSettings, (err, promptData) => {
         return;
     }
     console.log(promptData);
+
+    if (promptData.source.includes('.csv')) {
+        prompt.get(downloadMax, (err, downloadMaxValue) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            promptData.maxConcurrent = downloadMaxValue;
+        });
+    } else {
+        promptData.maxConcurrent = '1';
+    }
     main(promptData);
 });
