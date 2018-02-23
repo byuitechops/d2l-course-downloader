8ea5dfa62c8bec6ea5df149f9de38373791deaba
Zachary Williams <wil11063@byui.edu>
2018-02-23 10:07:54 -0700

    Add check to let user know a file exists with the same name already

--- main.js
+++ main.js
@@ -4,30 +4,41 @@
 var asyncLib = require('async');
 var getCookies = require('./getCookies.js');
 var dlCourse = require('./dlCourse.js');
+var fs = require('fs');
 
 module.exports = (userData, finalCb) => {
 
-    /* Determine settings */
-    getCookies(userData, (err, cookies) => {
-        if (err) {
-            finalCb(err);
-            return;
-        }
-        userData.cookies = cookies;
-        userData.loginURL = `https://${userData.domain}.brightspace.com/d2l/login?noredirect=1`;
-        userData.name = '';
+    var potentialDownloadLocation = path.resolve('.', userData.downloadLocation);
 
-        asyncLib.mapLimit(userData.ous, 10, (ou, callback) => {
-            userData.ou = ou;
-            var dataClone = Object.assign({}, userData);
-            dlCourse(dataClone, callback);
-        }, (err, courses) => {
+    fs.readdir(potentialDownloadLocation, (err, files) => {
+        if (err) console.log(err);
+        if (files.includes(userData.name + '.zip')) {
+            console.log('File with the same name exists at download location - it will NOT be overwritten.');
+        }
+
+
+        /* Determine settings */
+        getCookies(userData, (err, cookies) => {
             if (err) {
                 finalCb(err);
-            } else {
-                finalCb(null, courses);
+                return;
             }
-        });
+            userData.cookies = cookies;
+            userData.loginURL = `https://${userData.domain}.brightspace.com/d2l/login?noredirect=1`;
+            userData.name = '';
 
+            asyncLib.mapLimit(userData.ous, 10, (ou, callback) => {
+                userData.ou = ou;
+                var dataClone = Object.assign({}, userData);
+                dlCourse(dataClone, callback);
+            }, (err, courses) => {
+                if (err) {
+                    finalCb(err);
+                } else {
+                    finalCb(null, courses);
+                }
+            });
+
+        });
     });
-}
+};
