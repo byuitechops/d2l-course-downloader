3ce86efc38d5ad4c660d1cf74211394ecff14d31
Danverde <gre12014@byui.edu>
2018-05-11 12:57:55 -0600

    linter fix

--- getCookies.js
+++ getCookies.js
@@ -1,18 +1,20 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 
 module.exports = async userData => {
-  const browser = await puppeteer.launch({headless:true})
-  const page = await browser.newPage()
-  await page.goto(`https://${userData.domain}.brightspace.com/d2l/login?noredirect=true`)
-  await page.type('#userName',userData.username)
-  await page.type('#password',userData.password)
-  await Promise.all([
-    page.waitForNavigation({
-      waitUntil:'networkidle0'
-    }),
-    page.click('[primary]'),
-  ])
-  const cookies = await page.cookies()
-  await browser.close()
-  return cookies
-}
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
+    await page.goto(`https://${userData.domain}.brightspace.com/d2l/login?noredirect=true`);
+    await page.type('#userName', userData.username);
+    await page.type('#password', userData.password);
+    await Promise.all([
+        page.waitForNavigation({
+            waitUntil: 'networkidle0'
+        }),
+        page.click('[primary]'),
+    ]);
+    const cookies = await page.cookies();
+    await browser.close();
+    return cookies;
+};
